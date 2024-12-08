7ff2cab693f61db054464e0a493fed00b855c1b5
Tejas Gund <tejasgund111@gmail.com>
2024-12-08 15:06:07 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 const connectToDb = require("./db/db");
@@ -11,6 +12,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello");
