2713da0172ce97135e9e6c365d662f7a69b34cbf
Tejas Gund <tejasgund111@gmail.com>
2024-12-08 21:39:09 +0530

    register captain done

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require('./routes/captain.routes');
+
 const cookieParser = require("cookie-parser");
 const app = express();
 
@@ -19,5 +21,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users",userRoutes);
+app.use("/captains",captainRoutes);
 
 module.exports = app;
